b3dd33b4ad3a5dd4256ad1895f6e7b260dc91ad5
deepakagg500 <deepakagg500@gmail.com>
2022-02-12 20:03:05 +0530

    added second screen

--- android/app/src/main/java/com/deepak/abcd/MainApplication.java
+++ android/app/src/main/java/com/deepak/abcd/MainApplication.java
@@ -7,9 +7,6 @@ import androidx.annotation.NonNull;
 
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import com.zmxv.RNSound.RNSoundPackage;
-import com.zmxv.RNSound.RNSoundPackage;
-import com.zmxv.RNSound.RNSoundPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
